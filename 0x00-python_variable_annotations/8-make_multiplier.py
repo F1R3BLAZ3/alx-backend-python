aa290ae5d5beac839ce979a7b4641a6e3a4d5be2
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2024-01-11 13:42:37 +0000

    Task 8,9

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""
+    Returns a function that multiplies a float by the specified multiplier.
+"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns a function that multiplies a float by the specified multiplier.
+
+    :param multiplier: The float multiplier.
+    :return: A function that takes a float and returns the product.
+    """
+    def multiplier_function(value: float) -> float:
+        return value * multiplier
+
+    return multiplier_function
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""
+    Returns a list of tuples containing elements from the input list
+    and their lengths.
+"""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Returns a list of tuples containing elements from the input list
+    and their lengths.
+
+    :param lst: An iterable of sequences.
+    :return: A list of tuples, each containing a sequence and its length.
+    """
+    return [(i, len(i)) for i in lst]
