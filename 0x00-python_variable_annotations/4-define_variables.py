6ba50fb148d7940c6da3aada5896db9dc3dc3c05
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2024-01-11 13:32:07 +0000

    Task 4

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
+# Variable 'a' of type int with value 1
 a: int = 1
+
+# Variable 'pi' of type float with value 3.14
 pi: float = 3.14
+
+# Variable 'i_understand_annotations' of type bool with value True
 i_understand_annotations: bool = True
+
+# Variable 'school' of type str with value "Holberton"
 school: str = "Holberton"
