5a156e3461819a9461ee3c9c4f138c90246ea9c1
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2024-01-11 13:14:28 +0000

    Task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers and returns their sum.
+
+    :param a: The first float number.
+    :param b: The second float number.
+    :return: The sum of a and b.
+    """
+    return a + b
+
+
+if __name__ == "__main__":
+    print(add(1.11, 2.22) == 1.11 + 2.22)
+    print(add.__annotations__)
