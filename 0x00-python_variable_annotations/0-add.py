1a0b55363d4b2a5c22a41bac7eec9b54cc52c587
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2024-01-11 13:26:05 +0000

    Task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -14,8 +14,3 @@ def add(a: float, b: float) -> float:
     :return: The sum of a and b.
     """
     return a + b
-
-
-if __name__ == "__main__":
-    print(add(1.11, 2.22) == 1.11 + 2.22)
-    print(add.__annotations__)
