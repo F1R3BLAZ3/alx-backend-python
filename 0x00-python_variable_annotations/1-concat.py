fa8e59b268abfdb0e35272a4cd1f02382432781c
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2024-01-11 13:27:28 +0000

    Task 1,2

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+
+"""
+    Concatenates two strings and returns the result.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    :param str1: The first string.
+    :param str2: The second string.
+    :return: The concatenated string.
+    """
+    return str1 + str2
--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+
+"""
+    Returns the floor of a float.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float.
+
+    :param n: The input float.
+    :return: The floor value as an integer.
+    """
+    return math.floor(n)
