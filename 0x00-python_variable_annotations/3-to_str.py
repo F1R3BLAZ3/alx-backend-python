fdf77cb9af33396eeef64ff989a574c6765e6f56
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2024-01-11 13:31:08 +0000

    Task 3,4

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+"""
+    Returns the string representation of a float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float.
+
+    :param n: The input float.
+    :return: The string representation of the input float.
+    """
+    return str(n)
