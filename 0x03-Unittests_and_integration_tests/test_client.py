ead62bcf2c2172fed0a272702640aee2af0d32bd
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2024-02-05 21:25:39 +0000

    Task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -35,6 +35,34 @@ class TestGithubOrgClient(unittest.TestCase):
 
         # Ensure that the result matches the expected value
         self.assertEqual(result, {"org_key": "org_value"})
+
+    def test_public_repos_url(self):
+        """
+        Test GithubOrgClient._public_repos_url.
+
+        Use patch as a context manager to mock GithubOrgClient.org.
+        Test that the result of _public_repos_url is the expected one
+        based on the mocked payload.
+        """
+        # Mock the payload returned by GithubOrgClient.org
+        mock_payload = {
+            "repos_url": "https://api.github.com/orgs/testorg/repos"}
+
+        # Use patch to mock the org property and return the mock_payload
+        with patch.object(
+                GithubOrgClient, 'org',
+                new_callable=PropertyMock,
+                return_value=mock_payload
+        ):
+            github_client = GithubOrgClient("testorg")
+
+            # Access the _public_repos_url property
+            result = github_client._public_repos_url
+
+            # Ensure that the result matches the expected value
+            # from the mocked payload
+            self.assertEqual(
+                result, "https://api.github.com/orgs/testorg/repos")
 
 
 if __name__ == "__main__":
