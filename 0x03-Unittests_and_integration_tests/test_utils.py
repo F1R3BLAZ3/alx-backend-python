f4db4b922bfc43b0746b82563a96b1858a427b5b
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2024-02-05 20:59:21 +0000

    Task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,7 @@
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from typing import Dict, Tuple
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -26,22 +27,18 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
     @parameterized.expand([
-        ({}, ("a",), KeyError("Key not found: 'a' in {}")),
-        ({"a": 1}, ("a", "b"), KeyError("Key not found: 'b' in {'a': 1}")),
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected_exception):
-        """
-        Test that `access_nested_map` raises a KeyError
-        for certain inputs.
-
-        Parameters:
-        - nested_map (dict): The nested dictionary to be accessed.
-        - path (tuple): The path to the nested value.
-        - expected_exception: The expected KeyError message.
-        """
-        with self.assertRaises(KeyError) as context:
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected_exception: Exception,
+    ) -> None:
+        """Tests `access_nested_map`'s exception raising."""
+        with self.assertRaises(expected_exception):
             access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), str(expected_exception))
 
 
 if __name__ == "__main__":
