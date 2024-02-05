a9fc4dfdf291ff8581d7ef38355a9787c9213b9b
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2024-02-05 20:51:45 +0000

    Task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -25,6 +25,24 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
+    @parameterized.expand([
+        ({}, ("a",), KeyError("Key not found: 'a' in {}")),
+        ({"a": 1}, ("a", "b"), KeyError("Key not found: 'b' in {'a': 1}")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_exception):
+        """
+        Test that `access_nested_map` raises a KeyError
+        for certain inputs.
+
+        Parameters:
+        - nested_map (dict): The nested dictionary to be accessed.
+        - path (tuple): The path to the nested value.
+        - expected_exception: The expected KeyError message.
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), str(expected_exception))
+
 
 if __name__ == "__main__":
     unittest.main()
